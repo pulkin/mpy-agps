f26afbe79f67cfdc5c851482f89aa35ac03c4b59
Artem <gpulkin@gmail.com>
2020-01-19 21:04:15 +0100

    Add setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import sdist_upip
+
+setup(name='micropython-agps',
+      version='0.1',
+      description='Assisted location services for MicroPython',
+      long_description=open("README.md", 'r').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/pulkin/mpy-agps',
+      author='pulkin',
+      author_email='gpulkin@gmail.com',
+      cmdclass={'sdist': sdist_upip.sdist},
+      py_modules=['agps'])
