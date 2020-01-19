e112df97f75822465b2c14421429c8a6bec9c8f9
Artem <gpulkin@gmail.com>
2020-01-19 21:05:43 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import sdist_upip
 
 setup(name='micropython-agps',
-      version='0.1',
+      version='0.1.1',
       description='Assisted location services for MicroPython',
       long_description=open("README.md", 'r').read(),
       long_description_content_type='text/markdown',
